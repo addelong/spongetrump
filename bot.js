2688a69dc242323c3f012282de0930c2e9b01f63
addelong <github@alandelong.com>
2017-11-02 02:47:44 -0400

    the bot

--- bot.js
+++ bot.js
@@ -1,10 +1,73 @@
+var Twit = require('twit');
 var memecanvas = require('memecanvas-prebuilt');
+var fs = require('fs');
+
 memecanvas.init('./', '-meme');
-memecanvas.generate('./sb.jpg', 'tHiS iS a', 'TeSt', function(error, memefilename){
-	if(error){
-		console.log(error);
+
+var T = new Twit({
+ consumer_key: process.env.BOT_CONSUMER_KEY,
+ consumer_secret: process.env.BOT_CONSUMER_SECRET,
+ access_token: process.env.BOT_ACCESS_TOKEN,
+ access_token_secret: process.env.BOT_ACCESS_TOKEN_SECRET
+});
+
+function spongeify(words) {
+	var caseFlipper = true;
+	var res = "";
+	for (var i = 0; i < words.length; i++){
+		var word = words[i];
+		var spongeWord = "";
+		for (var j = 0; j < word.length; j++){
+			if (caseFlipper){
+				spongeWord += word[j].toUpperCase();
+			}
+			else {
+				spongeWord += word[j].toLowerCase();
+			}
+		}
+		res = res + spongeWord + " ";
+		caseFlipper = !caseFlipper;
 	}
-	else{
-		console.log(memefilename);
-	}
+	return res;
+}
+
+var stream = T.stream('statuses/filter', {follow: 25073877 });
+stream.on('tweet', function(tweet) {
+	var tweetText = tweet.text.toLowerCase();
+	var tweetWords = tweetText.split(' ');
+	var midpoint = Math.ceil(tweetWords.length / 2);
+	var topWords = tweetWords.slice(0, midpoint);
+	var bottomWords = tweetWords.slice(midpoint);
+
+	topWords = spongeify(topWords).trim();
+	bottomWords = spongeify(bottomWords).trim();
+
+	memecanvas.generate('./sb.jpg', topWords, bottomWords, function(error, memefilename){
+		if(error){
+			console.log(error);
+		}
+		else{
+			var b64content = fs.readFileSync('./sb-meme.jpg', {encoding: 'base64'});
+			T.post('media/upload', { media_data: b64content }, function (err, data, response) {
+    		if (err){
+      		console.log('ERROR:');
+      		console.log(err);
+					fs.unlinkSync('./sb-meme.jpg');
+    		}
+    		else {
+      		T.post('statuses/update', {
+        		media_ids: new Array(data.media_id_string)
+      		},
+        	function(err, data, response) {
+          	if (err){
+            	console.log('ERROR:');
+            	console.log(err);
+          	}
+
+						fs.unlinkSync('./sb-meme.jpg');
+        	});
+    		}
+  		});
+		}
+	});
 });
